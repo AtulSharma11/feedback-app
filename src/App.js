375f318916c6f52f2f7b90a375845b5cdaa3b780
Atul <atulsharma.sa1@gmail.com>
2022-06-10 19:23:51 -0400

    Practice(React) init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "blog post";
+  const body = "This is a blog post";
+  const comments = [
+    { id: 1, text: "comment one" },
+    { id: 2, text: "comment two" },
+    { id: 3, text: "comment three" },
+  ];
+
+  const loading = false;
+  const showComment = true;
+
+  if (loading) return <h1>Loading....</h1>;
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments: ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}> {comment.text} </li>
+        ))}
+      </ul>
+    </div>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComment && commentBlock}
     </div>
   );
 }
